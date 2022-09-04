e7d55d2d9bb36ba7f084f009d940ac83da7675e8
rojxarano <arano.rocio.b@gmail.com>
2022-09-03 21:31:27 -0300

    footer y boton de ingresar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SkillsComponent } from './components/skills/skills.component';
 import { SSkillsComponent } from './components/s-skills/s-skills.component';
 import { IdiomasComponent } from './components/idiomas/idiomas.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { ProyectosComponent } from './components/proyectos/proyectos.component';
     SkillsComponent,
     SSkillsComponent,
     IdiomasComponent,
-    ProyectosComponent
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
